71aaec245ca6ddc33b98a2e60daa68f4090390a8
王国栋 <beiyoujun@gmail.com>
2017-05-26 10:06:32 +0800

    更改时间显示

--- src/page/system/place/place.jsx
+++ src/page/system/place/place.jsx
@@ -1,12 +1,11 @@
 
-"use strict"
+import moment from 'moment'
 import React from 'react';
-
 import {connect} from 'dva';
 import './place.scss';
-import {Table,Input,Icon,Button,Popconfirm,Pagination} from 'antd';
-import {routerRedux} from 'dva/router';
-import {Link} from 'react-router';
+import { Table,Input,Icon,Button,Popconfirm,Pagination } from 'antd';
+import { routerRedux } from 'dva/router';
+import { Link } from 'react-router';
 import Current from '../../Current'
 
 class localCharIndex extends React.Component {
@@ -32,7 +31,9 @@ class localCharIndex extends React.Component {
       }, {
         title: '最后编辑时间',
         dataIndex: 'modifyTime',
-        key: 'modifyTime'
+        render: (record) => {
+          return moment(record).format("YYYY-MM-DD HH:mm:ss")
+        }
       }, {
         title: '操作',
         dataIndex: 'operation',
