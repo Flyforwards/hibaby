ce6530c3d5cb3cf0272578c92fc0fb8faac50784
gengen <18363850937@163.com>
2017-06-05 16:40:27 +0800

    个人中心修改密码功能去掉code!=0的时候的else.

--- src/models/rePassword.js
+++ src/models/rePassword.js
@@ -9,12 +9,10 @@ export default {
   effects: {
     //修改密码
     *updatePsw({ payload: values }, { call }) {
-      const { data: { code, err } } = yield call(rePswService.updatePassword, values);
+      const { data: { code } } = yield call(rePswService.updatePassword, values);
       if (code == 0) {
         message.success('修改成功');
         browserHistory.push('/login');
-      } else {
-        message.error(err)
       }
     }
   }
