f8dd868eaea9d709a295a698a07742abaa83090e
Flyforwards <yufy0921@126.com>
2017-06-08 16:50:58 +0800

    tab转换

--- src/page/crm/membershipcard/membershipcardIndex.js
+++ src/page/crm/membershipcard/membershipcardIndex.js
@@ -144,9 +144,19 @@ class MemberShipCard extends Component {
       flagValue:key,
     })
   }
+  //tab点击
+  onTabClick(key){
+   this.props.dispatch({
+     type:'membershipcard/getChangeTabKey',
+     payload:{
+       activeKeys:key,
+     }
+   })
+  }
+
 
   render() {
-    const { feeRecord,renewRecord,refundRecord } = this.props;
+    const { feeRecord,renewRecord,refundRecord,activeKey } = this.props;
     return (
       <div className="card" style={{overflow:'hidden'}}>
         <div>
@@ -157,7 +167,7 @@ class MemberShipCard extends Component {
           <div>
 
           </div>
-          <Tabs onChange={this.tabChange.bind(this)} type="card">
+          <Tabs activeKey={activeKey} onChange={this.tabChange.bind(this)} type="card" onTabClick={this.onTabClick.bind(this)}>
             <TabPane tab="扣费记录" key="1">
                 <ChargebackRecord />
             </TabPane>
@@ -226,11 +236,12 @@ class MemberShipCard extends Component {
 
 
 function mapStateToProps(state) {
-  const { feeRecord,renewRecord,refundRecord } = state.membershipcard;
+  const { feeRecord,renewRecord,refundRecord ,activeKey} = state.membershipcard;
   return {
     feeRecord,
     renewRecord,
     refundRecord,
+    activeKey,
     user:state.addCustomer,
     cards:state.membershipcard,
   };
