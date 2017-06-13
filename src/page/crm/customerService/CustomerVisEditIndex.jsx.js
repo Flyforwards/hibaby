d49832d2601081bf01b0e2dd9fa9812082efd6fb
gengen <18363850937@163.com>
2017-06-13 18:22:30 +0800

    参观档期完善样式。

--- src/page/crm/customerService/CustomerVisEditIndex.jsx.js
+++ src/page/crm/customerService/CustomerVisEditIndex.jsx.js
@@ -5,7 +5,7 @@
 import React from 'react'
 import { connect } from 'dva'
 import './CustomerVisIndex.scss'
-import { Table,Input,Icon,Button,Popconfirm, Modal, DatePicker, Card,Timeline, Row, Col } from 'antd'
+import { Table, Input, Icon, Button, Popconfirm, Modal, DatePicker, Card, Timeline, Row, Col } from 'antd'
 import { routerRedux } from 'dva/router'
 import { Link } from 'react-router'
 import moment from 'moment'
@@ -14,12 +14,12 @@ const confirm = Modal.confirm;
 const TimeItem = Timeline.Item
 
 class CustomerVisEditIndex extends React.Component {
-
+  
   constructor(props) {
     super(props);
-    this.removeIds=[];
+    this.removeIds = [];
   }
-
+  
   deleteCustomerComp(record) {
     const { dispatch } = this.props;
     confirm({
@@ -32,11 +32,11 @@ class CustomerVisEditIndex extends React.Component {
         })
       },
       onCancel() {
-      },
+      }
     });
   }
-
-
+  
+  
   onChangeDate(date) {
     const { dispatch } = this.props;
     this.removeIds = [];
@@ -49,10 +49,10 @@ class CustomerVisEditIndex extends React.Component {
       payload: { visDate: date.format('YYYY-MM-DD') }
     })
   }
-
+  
   save() {
     let ids = '';
-    this.removeIds.map((record, index)=>{
+    this.removeIds.map((record, index) => {
       if (index == 0) {
         ids = String(record);
       } else {
@@ -65,7 +65,7 @@ class CustomerVisEditIndex extends React.Component {
       payload: { ids }
     })
   }
-
+  
   remove(item) {
     if (!this.removeIds.contains(item.id)) {
       this.removeIds.push(item.id);
@@ -75,16 +75,18 @@ class CustomerVisEditIndex extends React.Component {
       payload: { itemId: item.id }
     })
   }
+  
   render() {
     const { list, dispatch, date } = this.props;
-    const times =  VISIT_TIME.map((record, index)=>{
+    const times = VISIT_TIME.map((record, index) => {
       let btns = []
-      list.map((item)=>{
-        if (index+1 == item.visitTimeId) {
-          btns.push(<span key={item.id} className="left-time-div"><span className="left-time-span" key={item.id}>{ item.name }</span><Icon onClick={ this.remove.bind(this,item)} type="close-circle"/></span>)
+      list.map((item) => {
+        if (index + 1 == item.visitTimeId) {
+          btns.push(
+            <span key={item.id} className="left-time-div"><span className="left-time-span" key={item.id}>{ item.name }</span><Icon onClick={ this.remove.bind(this, item)} type="close-circle"/></span>)
         }
       })
-
+      
       return (<TimeItem key={index}><span>{record}</span>
         <div>
           {
@@ -93,29 +95,36 @@ class CustomerVisEditIndex extends React.Component {
         </div>
       </TimeItem>)
     })
-
-
+    
+    
     const add = !this.props.permissionAlias.contains('CUSTOMERCOMP_ADD');
     return (
-      <div className = "customer-vis-cent">
-        <div className = "button-wrapper">
-          <DatePicker format="YYYY-MM-DD" defaultValue={ date } onChange={this.onChangeDate.bind(this)}/>
-          <Link to = '/crm/customer-vis/add'>
-            <Button disabled={add} className="button-add BackBtn"> 预约参观 </Button>
-          </Link >
+      <div className="customer-vis-cent">
+        <div className="button-wrapper">
+          <Row style={{ height: 40 }}>
+            <Col span={20}>
+              <span>预约日期：</span>
+              <DatePicker style={{ width: 200 }} format="YYYY-MM-DD" defaultValue={ date } onChange={this.onChangeDate.bind(this)}/>
+            </Col>
+            <Col span={4}>
+              <Button disabled={add} className="button-add">
+                <Link to='/crm/customer-vis/add'> 预约参观 </Link>
+              </Button >
+            </Col>
+          </Row>
         </div>
-        <Card title = { date.format('YYYY年MM月DD日') } >
+        <Card title={ date.format('YYYY年MM月DD日') }>
           <Timeline>
             {
               times
             }
           </Timeline>
         </Card>
-        <div>
+        <div style={{marginTop:20}}>
           <Row>
             <Col offset={16} span={4}>
               <Link to='/crm/customer-vis'>
-                <Button className="BackBtn"> 返回 </Button>
+                <Button className="button-add"> 返回 </Button>
               </Link>
             </Col>
             <Col span={4}>
@@ -130,15 +139,15 @@ class CustomerVisEditIndex extends React.Component {
 
 function mapStateToProps(state) {
   const {
-    list,
-    date
-  } = state.customerVis;
+          list,
+          date
+        } = state.customerVis;
   const { permissionAlias } = state.layout;
   return {
     loading: state.loading,
     list,
     permissionAlias,
-    date,
+    date
   };
 }
 
