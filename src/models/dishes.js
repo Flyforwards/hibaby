7f267ba0930e71df7bc0ee6952bad1b960303fe8
yangjingjing <yangjingjing1118@163.com>
2017-06-14 14:56:36 +0800

    菜品管理

--- src/models/dishes.js
+++ src/models/dishes.js
@@ -13,6 +13,7 @@ export default {
   namespace: 'dishes',
 
   state: {
+    dishesPageList : [],
     initialValue : null
   },
   //加载页面
@@ -21,52 +22,52 @@ export default {
   },
   //调用服务器端接口
   effects: {
+    //获取菜品信息分页数据
+    *getDishesPageList({payload : values}, { call, put }){
+      const {data: { data, code,err} } = yield call(dishesService.getDishesPageList, values);
+      if (code == 0) {
+        //更新state
+        yield put({type:'setDishesPageList',payload:{data}} );
+      }
+    },
     //获取菜品详情
-    *getDishesById11({payload}, { call, put }){
+    *getDishesById({payload : values}, { call, put }){
       const {data: { data, code,err} } = yield call(dishesService.getDishesById, values);
       if (code == 0) {
         //更新state
         yield put({type:'setDishesDetail',payload:{data}} );
       }
-      const data = {
-        id : 1,
-        name : '红烧鲫鱼',
-        mvType : 1,
-        vdType : 1,
-        nodeId : 1,
-        IngredientsDOs : [{
-          id : 1,
-          dishesId : 1,
-          type : 1,
-          name : '土豆',
-          volume : 50
-        },{
-          id : 2,
-          dishesId : 1,
-          type : 2,
-          name : '西红柿',
-          volume : 50
-        },{
-          id : 3,
-          dishesId : 1,
-          type : 2,
-          name : '鸡蛋',
-          volume : 50
-        }]
-      };
-      put({type:'setDishesDetail',payload:{data}} );
     },
+    //保存菜品信息
+    *saveDishes({payload : values}, { call, put }){
+      const {data: { data, code,err} } = yield call(dishesService.saveDishes, values);
+      if (code == 0) {
+        //更新state
+        message.info("菜品信息保存成功");
+        yield put({type:'setDishesDetail',payload:{data}} );
+      }
+    },
+    //删除菜品信息
+    *deleteDishes({payload : values}, { call, put }){
+      const {data: { data, code,err} } = yield call(dishesService.deleteDishes, values);
+      if (code == 0) {
+        //更新state
+        message.success("删除客户成功");
+        yield put({
+          type: 'getDishesPageList',
+        });
+      }
+    },
+
   },
   //同步请求，更新state
   reducers: {
     setDishesDetail(state, { payload: data }){
-      debugger;
-      console.log("1111111111111111111111111");
       message.info("111111111111");
-      return {...state,data}
+      return {...state,initialValue: data}
     },
     clearDishesDetail(state){
-      return {...state,initialValue : null}
+      return {...state,initialValue: null}
     }
   }
 };
--- src/services/dishes.js
+++ src/services/dishes.js
@@ -3,9 +3,31 @@
  * Created by yangjingjing on 2017/6/12.
  */
 import request from '../utils/request';
-export function saveHealthInformation(values) {
-  return request('/crm/api/v1/customer/saveCustomerHealth', {
+//获取菜品详情
+export function getDishesById(values) {
+  return request('/crm/api/v1/dishes/getDishesById', {
     method: 'POST',
     body: JSON.stringify(values),
   })
 };
+//获取菜品分页数据
+export function getDishesPageList(values){
+  return request('/crm/api/v1/dishes/getDishesPageList',{
+    method: 'POST',
+    body: JSON.stringify(values),
+  })
+}
+//保存菜品信息
+export function saveDishes(values){
+  return request('/crm/api/v1/dishes/saveDishes',{
+    method: 'POST',
+    body: JSON.stringify(values),
+  })
+}
+//删除菜品信息
+export function deleteDishes(values){
+  return request('/crm/api/v1/dishes/deleteDishes',{
+    method: 'POST',
+    body: JSON.stringify(values),
+  })
+}
