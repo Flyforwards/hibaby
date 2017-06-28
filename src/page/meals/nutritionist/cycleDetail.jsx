4d6ea7fcabc88d6f2988e40d9ce2861dd9ffd0dc
黄鑫 <2226401664@qq.com>
2017-06-28 18:32:46 +0800

    循环页面数据调试

--- src/page/meals/nutritionist/cycleDetail.jsx
+++ src/page/meals/nutritionist/cycleDetail.jsx
@@ -3,75 +3,103 @@ import { connect } from 'dva'
 import styles from './Cycle.scss';
 import { Form, Icon, Col, Row, Modal, Card, Tabs, Radio, Button, Badge } from 'antd'
 
-const  normalUsers = [
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1001'},
-];
-
-const  tabooUsers = [
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1001',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '大梅',roomNum: '1002',taboo: '汤、胡萝卜'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1003',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1004',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1005',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1006',taboo: '汤、胡萝卜'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1007',taboo: '汤、胡萝卜'},
-  {VIP: 'V2',userName: '赵大宝',roomNum: '1008',taboo: '汤、胡萝卜'},
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1001',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '大梅',roomNum: '1002',taboo: '汤、胡萝卜'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1003',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1004',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '赵大宝',roomNum: '1008',taboo: '汤、胡萝卜'},
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1001',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '大梅',roomNum: '1002',taboo: '汤、胡萝卜'},
-  {VIP: 'V2',userName: '赵丽颖',roomNum: '1003',taboo: '胡萝卜、芹菜、柠檬'},
-  {VIP: 'V2',userName: '红霄霄',roomNum: '1004',taboo: '胡萝卜、芹菜、柠檬'},
-];
-
 function mainCycleDetail(props) {
 
   function headerComponent (){
     return (
+
       <div>
-        <div style={{marginLeft: '85%' ,marginTop: '20px'}}><p style={{marginTop: '10px'}}>今日用餐客户共计<font color="#ac672c">40</font>人</p></div>
-        <div style={{marginTop: '-40px', marginRight: '20px'}}>{headerDateTabs()}</div>
+        <Row type="flex" justify="space-around" align="middle">
+          <Col span="1">
+            <Button
+              icon="double-left"
+              style={{height: '40px'}}
+              onClick={prevDateComponent}
+            />
+          </Col>
+          <Col span="22">
+            <div className="dayTitle">{props.loopWeekDatas.date} {chineseWeekDay(props.loopWeekDatas.dayOfWeek)}</div>
+            <div style={{marginLeft: '85%' ,marginTop: '-55px'}}><p>今日用餐客户共计<font color="#ac672c">{props.loopWeekDatas.allTotal}</font>人</p></div>
+            <div style={{marginTop: '20px' ,marginBottom: '40px'}}>{ mainComponent() }</div>
+          </Col>
+          <Col span="1">
+            <Button
+              icon="double-right"
+              style={{height: '40px', marginLeft: '20px'}}
+              onClick={nextDateComponent}
+            />
+          </Col>
+        </Row>
       </div>
     )
   }
 
-  // 头部日期切换
-  const TabPane = Tabs.TabPane;
-  function headerDateTabs () {
-    return (
-      <div className="cycle-tabs">
-        <Tabs defaultActiveKey='1'
-              activeKey={props.curTabsIndex}
-              onPrevClick={() => { prevDateComponent() }}
-              onNextClick={() => { nextDateComponent() }}
-        >
-          <TabPane tab="2017-06-11 周日" key="1"> { mainComponent() } </TabPane>
-          <TabPane tab="2017-06-12 周一" key="2"> { mainComponent() } </TabPane>
-          <TabPane tab="2017-06-13 周二" key="3"> { mainComponent() } </TabPane>
-          <TabPane tab="2017-06-14 周三" key="4"> { mainComponent() } </TabPane>
-          <TabPane tab="2017-06-15 周四" key="5"> { mainComponent() } </TabPane>
-          <TabPane tab="2017-06-16 周五" key="6"> { mainComponent() } </TabPane>
-        </Tabs>
-      </div>
+  //英文周几对应的汉字周几
+  function chineseWeekDay(dayOfWeek){
+    var chinessWeek = '';
+    if (dayOfWeek == 'Sunday'){
+      chinessWeek = '周日';
+    }
+    else if (dayOfWeek == 'Monday'){
+      chinessWeek = '周一';
+    }
+    else if (dayOfWeek == 'Tuesday'){
+      chinessWeek = '周二';
+    }
+    else if (dayOfWeek == 'Wednesday'){
+      chinessWeek = '周三';
+    }
+    else if (dayOfWeek == 'Thursday'){
+      chinessWeek = '周四';
+    }
+    else if (dayOfWeek == 'Friday'){
+      chinessWeek = '周五';
+    }
+    else if (dayOfWeek == 'Saturday'){
+      chinessWeek = '周六';
+    }
+
+    return(
+      chinessWeek
     )
   }
 
   function mainComponent () {
+
+    let loopObj = {};
+    if (props.loopWeekDatas.loops && props.loopWeekDatas.loops.length > 0){
+      loopObj = props.loopWeekDatas.loops[0];
+    }
+    //标准菜品
+    const normalObj = loopObj.loopTotals ? loopObj.loopTotals[0] : {} ;
+    const normalCustomers = normalObj.customers ? normalObj.customers : [] ;
+
+    //禁忌菜品
+    const tabooObj = loopObj.loopTotals ? loopObj.loopTotals[1] : {} ;
+    const tabooCustomers = tabooObj.customers ? tabooObj.customers : {} ;
+
+    let title = '周期一';
+    if (loopObj.week == 1) {
+      title = '周期一';
+    }
+    else if (loopObj.week == 2){
+      title = '周期二';
+    }
+    else if (loopObj.week == 3){
+      title = '周期三';
+    }
+    else if (loopObj.week == 4){
+      title = '周期四';
+    }
+    else if (loopObj.week == 5){
+      title = '周期五';
+    }
+
     return (
-      <div style={{marginLeft: '10px', marginTop: '20px'}}>
-        <Card title="周期一" bordered={true} style={{marginBottom: '20px', height: '100%', paddingBottom: '10px'}}>
+      <div>
+        <Card title={title} bordered={true} style={{marginBottom: '20px', height: '100%', paddingBottom: '10px'}}>
           <Row style={{display: 'flex',justifyContent: 'flex-end'}}>
-            <Badge count={3} style={{
+            <Badge count={loopObj.day} style={{
               backgroundColor: '#fff',
               color: '#999',
               boxShadow: '0 0 0 1px #d9d9d9 inset',
@@ -80,29 +108,29 @@ function mainCycleDetail(props) {
             />
           </Row>
           <Row>
-            <div className="cardSubtitle" style={{color:'#a5a5a5'}}>标准用户14人</div>
+            <div className="cardSubtitle" style={{color:'#a5a5a5'}}>标准用户{normalObj.total}人</div>
           </Row>
-          { normalUserListComponnet() }
+          { normalUserListComponnet(normalCustomers) }
           <Row>
             <div style={{background: '#e9e9e9', marginLeft: '20px', marginRight: '20px', marginTop: '20px', height: '1px'}}></div>
           </Row>
           <Row>
-            <div className="cardSubtitle" style={{color:'#a5a5a5', height:'30px',lineHeight: '30px',marginTop: '10px'}}>禁忌用户18人</div>
+            <div className="cardSubtitle" style={{color:'#a5a5a5', height:'30px',lineHeight: '30px',marginTop: '10px'}}>禁忌用户{tabooObj.total}人</div>
           </Row>
-          { tabooUserListComponnet() }
+          { tabooUserListComponnet(tabooCustomers) }
         </Card>
       </div>
     )
   }
 
   // 标准用户列表
-  function normalUserListComponnet() {
+  function normalUserListComponnet(customers) {
     const normalUserDivs = [];
-    for (let i = 0; i < normalUsers.length; i++) {
-      const user = normalUsers[i];
+    for (let i = 0; i < customers.length; i++) {
+      const user = customers[i];
       normalUserDivs.push(
         <Col span="4" key={i}>
-          <div className="tabooUser-content">{user['VIP']}  {user['userName']} -- {user['roomNum']}</div>
+          <div className="tabooUser-content">V{user['level']}  {user['name']} -- {user['room']}</div>
         </Col>
       );
     }
@@ -117,17 +145,21 @@ function mainCycleDetail(props) {
   }
 
   //禁忌用户列表
-  function tabooUserListComponnet() {
+  function tabooUserListComponnet(customers) {
     const tabooUserDivs = [];
-    for (let i = 0; i < tabooUsers.length; i++) {
-      const user = tabooUsers[i];
+    for (let i = 0; i < customers.length; i++) {
+      const user = customers[i];
       tabooUserDivs.push(
         <Row key={i} style={ i%2 ?{backgroundColor :'#ffffff'} :{backgroundColor: '#f6f6f6'}}>
           <Col span="5">
-            <div className="tabooUser-detail-content">{user['VIP']}  {user['userName']} -- {user['roomNum']}</div>
+            <div style={{minHeight: '40px'}}>
+              <div className="tabooUser-detail-content">V{user['level']}  {user['name']} -- {user['room']}</div>
+            </div>
           </Col>
           <Col>
-            <div className="tabooUser-detail-content">禁忌: {user['taboo']}</div>
+            <div style={{minHeight: '40px'}}>
+              <div className="tabooUser-detail-content">禁忌: {user['taboo']}</div>
+            </div>
           </Col>
         </Row>
       );
@@ -147,12 +179,27 @@ function mainCycleDetail(props) {
 
     var that = this;
     const {dispatch} = props;
-    setTimeout(function(){
-      dispatch({
-        type: 'cyclePage/changedDetailTabActivity',
-        payload: { status: 1 }
-      })
-    }.bind(that), 450);
+    dispatch({
+      type: 'cyclePage/changedDetailTabActivity',
+      payload: { status: 1 }
+    })
+
+    let loopObj = {};
+    if (props.loopWeekDatas.loops && props.loopWeekDatas.loops.length > 0){
+      loopObj = props.loopWeekDatas.loops[0];
+    }
+    var curWeek = loopObj.week;
+    curWeek = curWeek+1;
+    if (curWeek > 5){
+      curWeek = 1;
+    }
+
+    //查询单个循环的数据
+    dispatch({
+      type: 'cyclePage/getLoopListByWeek',
+      payload: { week: curWeek }
+    })
+
   }
 
   //前一天
@@ -160,22 +207,33 @@ function mainCycleDetail(props) {
 
     var that = this;
     const {dispatch} = props;
-    setTimeout(function(){
-      dispatch({
-        type: 'cyclePage/changedDetailTabActivity',
-        payload: { status: -1 }
-      })
-    }.bind(that), 450);
+    dispatch({
+      type: 'cyclePage/changedDetailTabActivity',
+      payload: { status: -1 }
+    })
+
+    let loopObj = {};
+    if (props.loopWeekDatas.loops && props.loopWeekDatas.loops.length > 0){
+      loopObj = props.loopWeekDatas.loops[0];
+    }
+    var curWeek = loopObj.week;
+    curWeek = curWeek-1;
+    if (curWeek < 1){
+      curWeek = 5;
+    }
+
+    //查询单个循环的数据
+    dispatch({
+      type: 'cyclePage/getLoopListByWeek',
+      payload: { week: curWeek }
+    })
+
   }
 
   function handleChangedState(){
     const {dispatch} = props;
     dispatch({
       type: 'cyclePage/changedShowStatus',
-    })
-    dispatch({
-      type: 'cyclePage/changedDetailTabActivity',
-      payload: { status: 0 }
     })
   }
 
