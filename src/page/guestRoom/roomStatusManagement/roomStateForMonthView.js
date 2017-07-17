275c64f91f650846c61050a7e621eaff21bfae14
李宏伟 <547383422@qq.com>
2017-07-17 17:39:32 +0800

    no message

--- src/page/guestRoom/roomStatusManagement/roomStateForMonthView.js
+++ src/page/guestRoom/roomStatusManagement/roomStateForMonthView.js
@@ -188,7 +188,7 @@ const monthStateView = (props) => {
       };
       const creatCheckbox = (month) => {
         return(
-          <Col span={4}><Checkbox  value={month}>{month}月</Checkbox></Col>
+          <Col key={month} span={4}><Checkbox  value={month}>{month}月</Checkbox></Col>
         )
       }
 
@@ -197,7 +197,7 @@ const monthStateView = (props) => {
       for(let i = 1 ;i<=12;i++){
         tempAry.push(creatCheckbox(i.toString()))
         if(i%6 == 0){
-          RowAry.push(<Row gutter={16} style={{height: "50%"}} type="flex" align="middle">{tempAry}</Row>)
+          RowAry.push(<Row key={'row'+i} gutter={16} style={{height: "50%"}} type="flex" align="middle">{tempAry}</Row>)
           tempAry = []
         }
       }
@@ -328,7 +328,7 @@ const monthStateView = (props) => {
         {
           statusExplain.map(item => {
             return (
-              <span>
+              <span key={item.name}>
                 <div style={{float: 'left'}}>{item.name}</div>
                 <div className="statusItem" style={{background: item.color}}/>
               </span>
@@ -608,6 +608,18 @@ const monthStateView = (props) => {
                 return;
               }
             }
+            else if (tempUnit < 0) {
+              if(status == 4){
+                let roomDate = roomList[roomIndex].useAndBookingList[oldEndIndex + tempUnit].date;
+
+                console.log(moment.unix(roomDate/1000).format('YYYY-MM-DD'))
+
+                  if(moment().isAfter(moment.unix(roomDate/1000),'day')){
+                  message.error("无法将出所日期移动到今天以前");
+                  return;
+                }
+              }
+            }
 
             unit = tempUnit;
 
@@ -695,7 +707,7 @@ const monthStateView = (props) => {
           + timeToDate(users[i].startDate + (users[i].dayCount - 1) * 86400000)
           + ')'}</div>
           result.push(
-            <Popover content={content} getPopupContainer={(e) => e} overlayClassName="popover-manual-top" arrowPointAtCenter={true}>
+            <Popover key={'pop'+i} content={content} getPopupContainer={(e) => e} overlayClassName="popover-manual-top" arrowPointAtCenter={true}>
               <div className="userBox"
                    style={{
                      width: width,
@@ -768,7 +780,7 @@ const monthStateView = (props) => {
 
         for (let i = 1; i <= fiveDays; i++) {
           result.push(
-            <div className="fiveDayRuler" style={{
+            <div key={'fiveDayRuler'+i} className="fiveDayRuler" style={{
               width: 5 * UNIT_WIDTH + "px",
             }}>
               {i * 5}天
@@ -778,7 +790,7 @@ const monthStateView = (props) => {
 
         if (remainder) {
           result.push(
-            <div className="fiveDayRuler" style={{
+            <div key={'remainder'+i} className="fiveDayRuler" style={{
               width: remainder * UNIT_WIDTH + "px",
             }}>
             </div>
