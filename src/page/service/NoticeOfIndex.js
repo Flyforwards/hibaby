0e2fe46440f867b2eb0fbec64d7a3b69e5a77d80
gengen <18363850937@163.com>
2017-09-01 12:38:22 +0800

    1，生成通知单。

--- src/page/service/NoticeOfIndex.js
+++ src/page/service/NoticeOfIndex.js
@@ -1,21 +1,23 @@
-
 import React, { Component } from 'react';
-import { Link} from 'react-router'
-import {Button} from 'antd';
+import { Link } from 'react-router'
+import { Button } from 'antd';
 
 /**
  * 通知单
  */
 class Index extends Component {
-
+  
   render() {
     return (
-
-      <Link to = '/service/send-message/production'>
-        <Button>生产通知单</Button>
-      </Link >
-
-
+      <div>
+        <Link to='/service/send-message/production'>
+          <Button>生产通知单</Button>
+        </Link >
+        <Link to='/service/stay-message/production'>
+          <Button>入住通知单</Button>
+        </Link >
+      
+      </div>
     )
   }
 }
