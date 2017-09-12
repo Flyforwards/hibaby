f311bb88b7a93edbeab11a3f389b554b4ccfc363
yangjingjing <yangjingjing1118@163.com>
2017-09-12 10:06:26 +0800

    预约游泳

--- src/page/service/order/SwimmingIndex.js
+++ src/page/service/order/SwimmingIndex.js
@@ -7,6 +7,8 @@ import {Card,Row,Col,Select,DatePicker} from 'antd';
 import { connect } from 'dva';
 import { Link } from 'react-router';
 import SwimmingIndexCss from  './SwimmingIndex.scss';
+import moment from  'moment'
+
 const Option = Select.Option;
 
 class SwimmingIndex extends Component{
@@ -16,51 +18,91 @@ class SwimmingIndex extends Component{
   }
 
 
-  initBox(){
-    return (
-      <Col span={6}>
-        <Row>
-          <Col span={8}>
-            <div className="ItemLeft">
-              <Link to="/service/order-swimming/detail">
-                <p>
-                  <img className="swimming-icon" src="http://test.file.hbbcare.com/image-60040ee0-fe12-40cb-b5dc-0fa08d57936d?Expires=1819960890&OSSAccessKeyId=LTAIhcIOePZxurct&Signature=Onu6PS0%2BMFx7cfMm3oF3%2FF0C8F8%3D" alt="" />
-                </p>
-                <p>小红</p>
-              </Link>
-              <Select className="swimming-select" defaultValue="1" placeholder="请选择">
-                <Option value="1">开工</Option>
-                <Option value="0">离开</Option>
-              </Select>
-            </div>
+
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'swimming/getSwimmingRooms',
+      payload:{
+        date : moment().format('YYYY-MM-DD')
+      }
+    })
+  }
+
+  handleDate=(value)=>{
+    const dateValue = moment(value).format('YYYY-MM-DD')
+    this.props.dispatch({
+      type: 'swimming/getSwimmingRooms',
+      payload:{
+        date : dateValue
+      }
+    })
+  }
+
+  handleChangeState=(value)=>{
+    this.props.dispatch({
+      type: 'swimming/changeSwimmingState',
+      payload:{
+        appointmentId : value.split(":")[0],
+        state : value.split(":")[1]
+      }
+    })
+  }
+
+
+  initBox=(indexData)=>{
+    const _this = this;
+    const boxArr = indexData?
+      indexData.map(function (item,index) {
+
+        const dateArr = item.dates?item.dates.map(function (index,dataValue) {
+          return <p>{dataValue}</p>;
+        }):null;
+
+        return (
+          <Col span={6}>
+            <Row>
+              <Col span={8}>
+                <div className="ItemLeft">
+                  <Link  to={`/service/order-swimming/detail?appointmentId=${item.appointmentId}&date=${moment().format('YYYY-MM-DD')}&type=0`}>
+                    <p>
+                      <img className="swimming-icon" src="http://test.file.hbbcare.com/image-60040ee0-fe12-40cb-b5dc-0fa08d57936d?Expires=1819960890&OSSAccessKeyId=LTAIhcIOePZxurct&Signature=Onu6PS0%2BMFx7cfMm3oF3%2FF0C8F8%3D" alt="" />
+                    </p>
+                    <p>{item.describe}</p>
+                  </Link>
+                  <Select className="swimming-select" defaultValue={item.appointmentId+":"+item.state} placeholder="请选择" onChange={_this.handleChangeState.bind(item.appointmentId)}>
+                    <Option value={item.appointmentId+":"+1}>开工</Option>
+                    <Option value={item.appointmentId+":"+0}>离开</Option>
+                  </Select>
+                </div>
+              </Col>
+              <Col span={14}>
+                <div className="ItemRight">
+                  {dateArr}
+                </div>
+              </Col>
+            </Row>
           </Col>
-          <Col span={14}>
-            <div className="ItemRight">
-              <p>9:00-9:30</p>
-              <p>9:00-9:30</p>
-              <p>9:00-9:30</p>
-              <p>9:00-9:30</p>
-              <p>9:00-9:30</p>
-            </div>
-          </Col>
-        </Row>
-      </Col>
-    );
+        );
+
+      }):null;
+
+    return boxArr;
+
+
   }
 
   render(){
+    const initBox = this.initBox();
+    const {indexData} = this.props;
     return (
       <Card className="order-swimming">
         <Row>
           <Col span={24} className="date-title">
-          <div><DatePicker/></div>
+          <div><DatePicker onChange={this.handleDate.bind(this)}/></div>
           </Col>
         </Row>
         <Row>
-          {this.initBox()}
-          {this.initBox()}
-          {this.initBox()}
-          {this.initBox()}
+          {this.initBox(indexData)}
         </Row>
       </Card>
     );
@@ -69,6 +111,7 @@ class SwimmingIndex extends Component{
 }
 function mapStateToProps(state) {
   return{
+    ...state.swimming,
     loading:state.loading,
   };
 }
