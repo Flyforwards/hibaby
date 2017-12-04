e75d9bee8efd279dd9105f8dbed0bd1a5cdc5520
Flyforwards <yufy0921@126.com>
2017-12-04 12:40:54 +0800

    xg

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -14,7 +14,7 @@ var webpackConfig = {
         historyApiFallback: true,
         inline: true,
         port: 9090,
-        //host: '192.168.199.81',
+       // host: '192.168.199.81',
         compress: true,
         // https://webpack.github.io/docs/node.js-api.html
         // https://github.com/webpack/webpack/blob/master/lib/Stats.js#L50
@@ -28,13 +28,13 @@ var webpackConfig = {
         proxy: {
 
             // 开发环境
-            '/crm/api/*': {
-                target: 'http://dev.hbbcare.com:8087/crm/api',
-                pathRewrite: {
-                    '^/crm/api': ''
-                },
-                changeOrigin: true
+          '/crm/api/*': {
+            target: 'http://dev.hbbcare.com:8087/crm/api',
+            pathRewrite: {
+              '^/crm/api': ''
             },
+            changeOrigin: true
+          },
           // // yfy
           // '/crm/api/*': {
           //   target: 'http://192.168.199.81:8087/crm/api',
