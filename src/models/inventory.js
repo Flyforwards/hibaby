e13fb2b379c903a4bdcf25c1b77394fb10836f31
gengen <18363850937@163.com>
2017-12-04 18:06:21 +0800

    1，存货明细完成版。

--- src/models/inventory.js
+++ src/models/inventory.js
@@ -7,62 +7,66 @@ import { parse } from 'qs'
 export default {
   namespace: 'inventory',
   state: {
-    pageList:null,
+    pageList: null,
     pagination: {
       showQuickJumper: true,
       showTotal: total => `共 ${total} 条`,
       current: 1,
-      pageSize:10,
-      total: null,
+      pageSize: 10,
+      total: null
     },
-    list:[],
+    list: [],
     inventoryPagination: {
       showQuickJumper: true,
       showTotal: total => `共 ${total} 条`,
       current: 1,
-      pageSize:10,
-      total: null,
-    },
+      pageSize: 10,
+      total: null
+    }
   },
   reducers: {
     //分页
-    getCardPageSave(state, { payload: { list, pagination }}) {
-      return {...state, list, pagination: {  ...state.pagination,...pagination }};
+    getCardPageSave(state, { payload: { list, pagination } }) {
+      return { ...state, list, pagination: { ...state.pagination, ...pagination } };
     },
-    getInventoryPageSave(state,{payload:{list,inventoryPagination}}){
-      return {...state, list, inventoryPagination: {  ...state.inventoryPagination,...inventoryPagination }};
+    getInventoryPageSave(state, { payload: { list, inventoryPagination } }){
+      return { ...state, list, inventoryPagination: { ...state.inventoryPagination, ...inventoryPagination } };
     },
-    saveWarehousePageList(state,{ payload : {data : pageList}}){
-      return { ...state,pageList}
+    saveWarehousePageList(state, { payload: { data: pageList } }){
+      return { ...state, pageList }
     },
     //保存仓库详情
-    saveWarehouseDetail(state,{payload:{ data:detailData}}){
-      return { ...state,detailData}
+    saveWarehouseDetail(state, { payload: { data: detailData } }){
+      return { ...state, detailData }
     },
     /*保存仓库明细列表*/
-    saveInventoryList(state,{payload:{data:inventoryData}}){
-      return { ...state,inventoryData}
+    saveInventoryList(state, { payload: data }){
+      let inventoryData = [];
+      data.length > 0 && data.map((v, k) => {
+        inventoryData.push(v.inventoryDO)
+      })
+      return { ...state, inventoryData }
     },
-
+    
     removeData(state, { payload: data }){
       return {
         ...state,
         detailData: null,
-        pageList: null,
+        pageList: null
       }
-    },
+    }
   },
   effects: {
     /*保存仓库*/
-    *saveWarehouse({payload:values},{call,put}){
-      const { data:{data,code}} = yield call(inventoryService.saveWarehouse,values);
-      if(code == 0) {
+    *saveWarehouse({ payload: values }, { call, put }){
+      const { data: { data, code } } = yield call(inventoryService.saveWarehouse, values);
+      if (code == 0) {
         message.success('仓库保存成功')
         yield put(routerRedux.push('/inventory/warehouse'))
       }
     },
     /*获取仓库列表*/
-    *getWarehousePageList({payload:values},{call,put}){
+    *getWarehousePageList({ payload: values }, { call, put }){
       values = parse(location.search.substr(1))
       if (values.page === undefined) {
         values.page = 1;
@@ -70,13 +74,13 @@ export default {
       if (values.size === undefined) {
         values.size = 10;
       }
-      const { data:{data,code,page,size,total}} = yield call(inventoryService.getWarehousePageList,values);
-      if(code == 0) {
+      const { data: { data, code, page, size, total } } = yield call(inventoryService.getWarehousePageList, values);
+      if (code == 0) {
         yield put({
-          type:'saveWarehousePageList',
-          payload:{
+          type: 'saveWarehousePageList',
+          payload: {
             data
-          },
+          }
         })
         yield put({
           type: 'getCardPageSave',
@@ -85,50 +89,41 @@ export default {
             pagination: {
               current: Number(page) || 1,
               pageSize: Number(size) || 10,
-              total: total,
-            },
-          },
+              total: total
+            }
+          }
         })
       }
     },
     /*根据Id查询仓库详情*/
-    *getWarehouseDetailById({payload:values},{call,put}){
-      const { data:{ data,code}} = yield call(inventoryService.getWarehouseDetailById,values);
-      if(code == 0){
+    *getWarehouseDetailById({ payload: values }, { call, put }){
+      const { data: { data, code } } = yield call(inventoryService.getWarehouseDetailById, values);
+      if (code == 0) {
         yield put({
-          type:'saveWarehouseDetail',
-          payload:{
+          type: 'saveWarehouseDetail',
+          payload: {
             data
           }
         })
       }
     },
-
+    
     /*删除成功*/
-    *deleteWarehouse({payload:values},{call,put}){
-      const { data:{data,code}} = yield call(inventoryService.deleteWarehouse,values);
-      if(code == 0){
+    *deleteWarehouse({ payload: values }, { call, put }){
+      const { data: { data, code } } = yield call(inventoryService.deleteWarehouse, values);
+      if (code == 0) {
         message.success("删除成功")
         yield put(routerRedux.push('/inventory/warehouse'))
       }
     },
-
+    
     /*仓库明细列表*/
-    *getInventoryList({payload:values},{call,put}){
-      values = parse(location.search.substr(1))
-      if (values.page === undefined) {
-        values.page = 1;
-      }
-      if (values.size === undefined) {
-        values.size = 10;
-      }
-      const { data:{ data,code}} = yield call(inventoryService.getInventoryList,values);
-      if(code == 0) {
+    *getInventoryList({ payload: values }, { call, put }){
+      const { data: { data, code,page,size ,total} } = yield call(inventoryService.getInventoryList, values);
+      if (code == 0) {
         yield put({
-          type:'saveInventoryList',
-          payload:{
-            data
-          }
+          type: 'saveInventoryList',
+          payload: data
         })
         yield put({
           type: 'getInventoryPageSave',
@@ -137,45 +132,46 @@ export default {
             inventoryPagination: {
               current: Number(page) || 1,
               pageSize: Number(size) || 10,
-              total: total,
-            },
-          },
+              total: total
+            }
+          }
         })
       }
     }
-
+    
   },
-
+  
   subscriptions: {
     setup({ dispatch, history }) {
-      return history.listen(({ pathname ,query}) => {
-        if(pathname === '/inventory/warehouse'){
+      return history.listen(({ pathname, query }) => {
+        if (pathname === '/inventory/warehouse') {
           dispatch({
-            type:'getWarehousePageList',
+            type: 'getWarehousePageList'
           })
         }
-        if(pathname === '/inventory/warehouse/detail'){
+        if (pathname === '/inventory/warehouse/detail') {
           dispatch({
-            type:'getWarehouseDetailById',
-            payload:{
-              "id":query.id
+            type: 'getWarehouseDetailById',
+            payload: {
+              "id": query.id
             }
           })
         }
-        if(pathname === '/inventory/warehouse/edit'){
-          if(query.id){
+        if (pathname === '/inventory/warehouse/edit') {
+          if (query.id) {
             dispatch({
-              type:'getWarehouseDetailById',
-              payload:{
-                "id":query.id
+              type: 'getWarehouseDetailById',
+              payload: {
+                "id": query.id
               }
             })
           }
         }
         /*仓库明细*/
-        if(pathname === '/inventory/warehouse-inventory'){
+        if (pathname === '/inventory/warehouse-inventory') {
           dispatch({
-            type:'getInventoryList'
+            type: 'getInventoryList',
+            payload: { 'size': 10, 'page': 1 }
           })
         }
       })
--- src/page/inventory/inventory/InventoryIndex.js
+++ src/page/inventory/inventory/InventoryIndex.js
@@ -2,10 +2,10 @@
  * Created by Flyforwards on 2017/11/21.
  */
 
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'dva'
-import { Card, Input, Button, Form, DatePicker, Row, Col,Table,Popconfirm } from 'antd';
-import { Link,routerRedux } from 'react-router';
+import { Card, Input, Button, Form, DatePicker, Row, Col, Table, Popconfirm } from 'antd';
+import { Link, routerRedux } from 'react-router';
 const FormItem = Form.Item;
 const createForm = Form.create
 import moment from 'moment'
@@ -13,160 +13,158 @@ import '../warehouse/inventoryIndex.scss'
 
 @createForm()
 class InventoryIndex extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-
-    }
+    this.state = {}
     this.columns = [{
       title: '存货编码',
       dataIndex: 'goodsId',
       key: 'goodsId'
     }, {
       title: '存货名称',
-      dataIndex: 'inventoryName',
-      key: 'inventoryName'
+      dataIndex: 'goodsName',
+      key: 'goodsName'
     }, {
       title: '辅助属性',
       dataIndex: 'resultValue',
       key: 'resultValue'
-    },{
+    }, {
       title: '规格型号',
       dataIndex: 'specificationModel',
       key: 'specificationModel'
     }, {
       title: '存货分类',
       dataIndex: 'inventoryClasses',
-      key: 'inventoryClasses',
-    },{
+      key: 'inventoryClasses'
+    }, {
       title: '计量单位',
       dataIndex: 'unit',
       key: 'unit'
-    },{
+    }, {
       title: '售价',
       dataIndex: 'price',
-      key: 'price'
-    },{
+      key: 'price',
+      render: (text, record, index) => {
+        return `${text}/件`
+      }
+    }, {
       title: '更新时间',
       dataIndex: 'operatorTime',
       key: 'operatorTime',
-      render:(text,record,index) => {
+      render: (text, record, index) => {
         return moment(text).format("YYYY-MM-DD")
       }
     }];
-
+    
   }
-
-  // //点击查看
-  // onLook(record){
-  //   this.props.dispatch(
-  //     routerRedux.push({
-  //       pathname:'/inventory/warehouse/detail',
-  //       query:{
-  //         id:record.id
-  //       }
-  //     })
-  //   )
-  // }
-  // //点击删除
-  // onDelete(id){
-  //   this.props.dispatch({
-  //     type:'inventory/deleteWarehouse',
-  //     payload:{
-  //       'id':id
-  //     }
-  //   })
-  // }
-//搜索
-  handleSearch(){
+  
+  //搜索
+  handleSearch() {
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        this.props.dispatch(routerRedux.push({
-          pathname: "/inventory/getInventoryList",
-          query: values
-        }))
+        
+        values.size = 10;
+        values.page = 1;
+        
+        this.props.dispatch({
+          type: 'inventory/getInventoryList',
+          payload: values
+        })
+        this.setState({
+          sear: values.sear
+        })
       }
     })
   }
-
+  
   //重置
-  onReset(){
-    const { pathname } = location;
-    this.props.dispatch(routerRedux.push({
-      pathname
-    }))
-    this.props.form.resetFields()
+  onReset() {
+    this.props.form.resetFields();
+    this.props.dispatch({
+      type: 'inventory/getInventoryList',
+      payload: { page: 1, size: 10 }
+    })
+    this.setState({
+      sear: null
+    })
   }
-
+  
   componentWillUnmount() {
     this.props.dispatch({ type: 'inventory/removeData' })
   }
-  render(){
-    const { inventoryPagination ,dispatch,inventoryData,loading} = this.props;
+  
+  render() {
+    const { inventoryPagination, dispatch, inventoryData, loading } = this.props;
     const { getFieldDecorator } = this.props.form;
-
-
+    
+    
     const formItemLayout = {
       labelCol: {
-        span:1
+        span: 1
       },
       wrapperCol: {
-        span:24
-      },
+        span: 24
+      }
     }
-
+    
     const tableProps = {
-      loading:loading,
-      pagination:inventoryPagination,
-      dataSource:inventoryData,
+      loading: loading,
+      pagination: inventoryPagination,
+      dataSource: inventoryData,
       onChange: (page) => {
         dispatch({
           type: 'inventory/getInventoryList',
-          payload: {
-            'page': page.current,
-            'size': page.pageSize,
-            'sortField': "operatorTime",
-            'sortOrder': "desc"
-          }
+          payload: this.state.sear ?
+            {
+              'page': page.current,
+              'size': page.pageSize,
+              'sear': this.state.sear,
+              'sortField': "operatorTime",
+              'sortOrder': "desc"
+            } : {
+              'page': page.current,
+              'size': page.pageSize,
+              'sortField': "operatorTime",
+              'sortOrder': "desc"
+            }
         })
       }
     };
-
+    
     return (
       <div className="inventoryIndex">
         <Card>
           <Row>
-            <Col span={12} style={{marginRight:'10px'}}>
+            <Col span={12} style={{ marginRight: '10px' }}>
               <Form >
-                <FormItem {...formItemLayout} label="" >
-                  {getFieldDecorator('sear', {rules: [{ required: false, message: '' }],
+                <FormItem {...formItemLayout} label="">
+                  {getFieldDecorator('sear', {
+                    rules: [{ required: false, message: '' }]
                   })(
-                    <Input placeholder="请输入存货名称搜索存货" />
+                    <Input placeholder="请输入存货名称搜索存货"/>
                   )}
                 </FormItem>
               </Form>
             </Col>
             <Col span={6}>
-              <Button className="button-group-bottom-2"  onClick={ this.handleSearch.bind(this) } style={{marginRight:'10px'}}> 搜索 </Button>
-              <Button className="button-group-bottom-3"  onClick={ this.onReset.bind(this) }> 重置 </Button>
+              <Button className="button-group-bottom-2" onClick={ this.handleSearch.bind(this) } style={{ marginRight: '10px' }}>
+                搜索 </Button>
+              <Button className="button-group-bottom-3" onClick={ this.onReset.bind(this) }> 重置 </Button>
             </Col>
           </Row>
           <Row>
-            <Table className="info-card-center" columns={this.columns} bordered rowKey="id" { ...tableProps }/>
+            <Table className="info-card-center" dataSource={inventoryData} columns={this.columns} bordered rowKey="id" { ...tableProps }/>
           </Row>
         </Card>
       </div>
     )
   }
-
+  
 }
 
 function mapStateToProps(state) {
-  const { inventoryPagination,inventoryData } = state.inventory;
-  return{
-    loading: state.loading.models.inventory,
-    inventoryPagination,
-    inventoryData
+  return {
+    ...state.inventory
   };
 }
 
